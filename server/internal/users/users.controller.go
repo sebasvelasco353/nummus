f6337c01bcbb32b55fac8c468f901fb1037f9683
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-07-03 18:28:59 -0500

    feat(database): added basic migration that creates the user table

--- server/internal/users/users.controller.go
+++ server/internal/users/users.controller.go
@@ -1,1 +1,10 @@
 package users
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func getUserById(context *gin.Context) {
+	fmt.Println("Hello")
+}
--- server/internal/users/users.routes.go
+++ server/internal/users/users.routes.go
@@ -1,1 +1,9 @@
 package users
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/users", getUserById)
+}
