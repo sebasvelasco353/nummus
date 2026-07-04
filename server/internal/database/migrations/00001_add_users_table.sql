f7c693220c8cb233ec8a5a8f2b4df13d2b792991
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-07-04 17:17:42 -0500

    feat(database): added basic migration that creates the accounts table

--- server/internal/database/migrations/00001_add_users_table.sql
+++ server/internal/database/migrations/00001_add_users_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS users (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT,
   last_name TEXT,
   email TEXT UNIQUE NOT NULL,
@@ -8,4 +8,4 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 -- +goose Down
-DROP TABLE users;
+DROP TABLE users CASCADE;
