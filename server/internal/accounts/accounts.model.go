739c1bd4b0273e91457f3f9a05a6cf807b4c50b8
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-09-21 16:52:26 -0500

    feat(server): added the type of account to the accounts table

--- server/internal/accounts/accounts.model.go
+++ server/internal/accounts/accounts.model.go
@@ -5,20 +5,21 @@ import (
 )
 
 type Account struct {
-	AccountId string
-	Owner     string
-	Bank      string `binding:"required"`
-	Name      string `binding:"required"`
-	Balance   int
-	Currency  string `binding:"required"`
+	AccountId   string
+	Owner       string
+	Bank        string `binding:"required"`
+	Name        string `binding:"required"`
+	Balance     int
+	Currency    string `binding:"required"`
+	AccountType string `binding:"required,oneof=savings debit credit"`
 }
 
 func (a Account) CreateAccount() (string, error) {
 	var err error
 	var accountId string
 
-	query := "INSERT INTO accounts (owner, bank, name, balance, currency) VALUES ($1, $2, $3, $4, $5) returning account_id"
-	err = config.DB.QueryRow(query, a.Owner, a.Bank, a.Name, a.Balance, a.Currency).Scan(&accountId)
+	query := "INSERT INTO accounts (owner, bank, name, balance, currency, account_type) VALUES ($1, $2, $3, $4, $5, $6) returning account_id"
+	err = config.DB.QueryRow(query, a.Owner, a.Bank, a.Name, a.Balance, a.Currency, a.AccountType).Scan(&accountId)
 	if err != nil {
 		return "", err
 	}
