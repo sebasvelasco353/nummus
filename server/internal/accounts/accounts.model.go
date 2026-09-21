ceac8c293fd450a9f34a8bdc3cf53d468549c6dd
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-09-21 18:18:56 -0500

    feat(server): added get account model functionality

--- server/internal/accounts/accounts.model.go
+++ server/internal/accounts/accounts.model.go
@@ -26,3 +26,13 @@ func (a Account) CreateAccount() (string, error) {
 	a.AccountId = accountId
 	return a.AccountId, nil
 }
+
+func GetAccount(accountId string, userId string) (Account, error) {
+	var account Account
+	var err error
+
+	query := "SELECT account_id, owner, bank, name, balance, currency, account_type FROM accounts WHERE account_id = $1 AND owner = $2"
+	err = config.DB.QueryRow(query, accountId, userId).Scan(&account.AccountId, &account.Owner, &account.Bank, &account.Name, &account.Balance, &account.Currency, &account.AccountType)
+
+	return account, err
+}
--- server/internal/database/migrations/00002_add_accounts_table.sql
+++ server/internal/database/migrations/00002_add_accounts_table.sql
@@ -3,7 +3,7 @@ CREATE TYPE account_types AS ENUM ('savings', 'debit', 'credit');
 
 CREATE TABLE IF NOT EXISTS accounts (
   account_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  owner UUID REFERENCES users(user_id) ON DELETE CASCADE,
+  owner UUID REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
   bank TEXT NOT NULL,
   name TEXT NOT NULL,
   balance INTEGER NOT NULL,
